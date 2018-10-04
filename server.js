b0c8927776f5cfbe7ad57107a09389cf6d953e4c
Dexdot <dexdotapb@gmail.com>
2018-10-05 01:44:31 +0300

    resolvers and queries

--- server.js
+++ server.js
@@ -1,23 +1,40 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+// Data
 const todos = [
   { task: 'Wash my car', completed: false },
   { task: 'Clean room', completed: true }
 ];
 
+// typeDefs
 const typeDefs = gql`
   type Todo {
     task: String
     completed: Boolean
   }
+
+  type Query {
+    getTodos: [Todo]
+  }
 `;
 
+// Resolvers
+const resolvers = {
+  Query: {
+    getTodos: () => todos
+  }
+};
+
+// Create Apollo Server
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  resolvers
 });
 
+// Port
 const port = 4000;
 
+// Start server
 server.listen(port).then(({ url }) => {
   console.log(`Server listening ${url}`);
 });
