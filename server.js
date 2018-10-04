935d763caa740f6223d026c89f5ee68459482b8c
Dexdot <dexdotapb@gmail.com>
2018-10-05 01:15:32 +0300

    server

---
+++ server.js
@@ -0,0 +1,23 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const todos = [
+  { task: 'Wash car', completed: false },
+  { task: 'Clean room', completed: true }
+];
+
+const typeDefs = gql`
+  type Todo {
+    task: String
+    completed: Boolean
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs
+});
+
+const port = 4000;
+
+server.listen(port).then(({ url }) => {
+  console.log(`Server listening ${url}`);
+});
