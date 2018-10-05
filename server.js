8c05e8c544dd76369b13ce3d1f8a81c33f5e8af7
Dexdot <dexdotapb@gmail.com>
2018-10-05 11:37:01 +0300

    mutation

--- server.js
+++ server.js
@@ -16,12 +16,24 @@ const typeDefs = gql`
   type Query {
     getTodos: [Todo]
   }
+
+  type Mutation {
+    addTodo(task: String, completed: Boolean): Todo
+  }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
     getTodos: () => todos
+  },
+  Mutation: {
+    addTodo: (_, { task, completed }) => {
+      const todo = { task, completed };
+      todos.push(todo);
+
+      return todo;
+    }
   }
 };
 
