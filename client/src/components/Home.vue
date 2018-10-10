5c36536ccc6a81229d6320e1054b74c002f5a381
Dexdot <dexdotapb@gmail.com>
2018-10-10 05:59:43 +0300

    mutations and getters

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div v-if="getPosts">
+  <div>
     <v-flex xs12 text-xs-center>
-      <v-carousel v-bind="{ 'cycle': true }" interval="3000">
-        <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
+      <v-carousel v-if="posts.length" v-bind="{ 'cycle': true }" interval="3000">
+        <v-carousel-item v-for="post in posts" :key="post._id" :src="post.imageUrl">
           <h1 class="carousel__title"> {{ post.title }} </h1>
         </v-carousel-item>
       </v-carousel>
@@ -16,34 +16,44 @@ import { gql } from 'apollo-boost';
 
 export default {
   name: 'Home',
-  data: () => ({
-    posts: []
-  }),
-  apollo: {
-    getPosts: {
-      query: gql`
-        query {
-          getPosts {
-            _id
-            title
-            description
-            imageUrl
-            categories
-            createdDate
-            likes
-            } 
-        }
-      `,
-      result({ data, loading }) {
-        if (!loading) {
-          this.posts = data.getPosts;
-        }
-      },
-      error(err) {
-        console.error(err);
-      }
+  methods: {
+    handleGetCarouselPosts() {
+      this.$store.dispatch('getPosts');
+    }
+  },
+  created() {
+    this.handleGetCarouselPosts();
+  },
+  computed: {
+    posts() {
+      return this.$store.getters.posts;
     }
   }
+  // apollo: {
+  //   getPosts: {
+  //     query: gql`
+  //       query {
+  //         getPosts {
+  //           _id
+  //           title
+  //           description
+  //           imageUrl
+  //           categories
+  //           createdDate
+  //           likes
+  //           } 
+  //       }
+  //     `,
+  //     result({ data, loading }) {
+  //       if (!loading) {
+  //         this.posts = data.getPosts;
+  //       }
+  //     },
+  //     error(err) {
+  //       console.error(err);
+  //     }
+  //   }
+  // }
 }
 </script>
 
